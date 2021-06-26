9ec29c21acf9091b81d111931af71db4053ad844
LiTer05 <83471426+LiTer05@users.noreply.github.com>
2021-06-26 04:29:28 +0400

    Add files via upload

---
+++ LinkedStack/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+#include "L_Stack.hpp"
+
+int main()
+{
+    LStack<int> st;
+    st.push(3);
+    st.push(7);
+    st.push(9);
+    st.push(1);
+
+    while(!st.empty())
+    {
+
+        std::cout << st.top() << " ";
+        st.pop();
+    }
+    std::cout << std::endl;
+}
+
